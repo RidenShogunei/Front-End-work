acd7c1e89b7dcc199767fc0c03c3dad7ff2240af
陈瑾旭 <108864288+RidenShogunei@users.noreply.github.com>
2024-02-29 16:12:41 +0800

    2,29

---
+++ src/views/HomePage.vue
@@ -0,0 +1,53 @@
+<template>
+    <div class="home" @click="navigate">
+        <h1>Welcome to my website</h1>
+        <h2>Press anywhere to enter</h2>
+    </div>
+</template>
+
+<script>
+export default {
+    methods: {
+        navigate() {
+            // 跳转
+            this.$router.push('/mainpage');
+        }
+    }
+}
+</script>
+
+<style>
+.home {
+  /* 之前的样式不变 */
+  background-image: url('../assets/background.jpg');
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+
+  position: fixed;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+
+  /* 使用 flexbox 将内容居中 */
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  /* flex-direction 为 column 可以使 h1 和 h2 垂直排列 */
+  flex-direction: column;
+}
+
+.home h1 {
+  font-size: 4em;
+  color: aliceblue;
+  /* 为 h1 添加底部边距 */
+  margin-bottom: 20px;
+}
+
+.home h2 {
+  /* 为 h2 添加样式 */
+  font-size: 3em;
+  color: rgb(230, 52, 21);
+}
+</style>
---
+++ src/views/mainPage.vue
@@ -0,0 +1,55 @@
+<template>
+    <div class="main">
+        <el-card class="box-card">
+            <div class="avatar">
+                <el-avatar :size="200" :src="require('../assets/me.jpg')" />
+            </div>
+            <h1>姓名: 陈瑾旭</h1>
+            <h1>学校：同济大学</h1>
+            <h1>专业：软件工程</h1>
+            <h1>兴趣爱好：编程，阅读，足球</h1>
+            <h1>联系方式：13870105832</h1>
+            <h1>个人简介：对新技术充满热情，善于解决问题，具有良好的团队合作精神。</h1>
+        </el-card>
+    </div>
+</template>
+
+<script>
+
+</script>
+
+<style>
+.main {
+    background-image: url('../assets/lrosrick.jpg');
+    background-size: cover;
+    background-position: center;
+    background-repeat: no-repeat;
+    position: fixed;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+}
+
+.box-card {
+    width: 500px;
+    height: 700px;
+    background-color: rgba(121, 134, 10, 0.033);
+    position: relative;
+    top: 10px;
+    left: 20px;
+    padding: 20px;
+    box-sizing: border-box;
+}
+
+.avatar {
+    display: flex;
+    justify-content: center;
+}
+
+
+h1, h2, h3, h4 {
+    margin: 10px 0;
+    color: rgb(222, 230, 220)
+}
+</style>
