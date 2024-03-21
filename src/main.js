4e5cf31bd42f9190c64fdfbace91f13685e67030
陈瑾旭 <108864288+RidenShogunei@users.noreply.github.com>
2024-03-21 11:24:10 +0800

    出了问题
    解决不了

--- src/main.js
+++ src/main.js
@@ -3,14 +3,14 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import ElementPlus from 'element-plus'
+import Antd from 'ant-design-vue';
 import 'element-plus/dist/index.css'
 import axios from 'axios';
 const app = createApp(App);
-
-// 将 Axios 实例挂载到 Vue 实例的原型上
 app.config.globalProperties.$axios = axios;
 
 app.use(store)
   .use(router)
   .use(ElementPlus)
+  .use(Antd)
   .mount('#app');
--- src/views/diaryPage.vue
+++ src/views/diaryPage.vue
@@ -1,48 +1,83 @@
 <template>
-    <div class="diary" >
-        <div>
-            
-        </div>
-    </div>
-  </template>
-  
-  <script>
-  export default {
-    data() {
-      return {
-      };
-    },
-    methods: {
-      navigate() {
-        // 跳转
-        this.$router.push('/mainpage');
-      }
-    },
-    created() {
-    
-    }
-  };
-  </script>
-  
-  <style scoped>
-  .diary {
-    /* 之前的样式不变 */
-    background-image: url('../assets/blacktiane.jpg');
-    background-size: cover;
-    background-position: center;
-    background-repeat: no-repeat;
-    position: fixed;
-    top: 0;
-    right: 0;
-    bottom: 0;
-    left: 0;
-    /* 使用 flexbox 将内容居中 */
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    /* flex-direction 为 column 可以使 h1 和 h2 垂直排列 */
-    flex-direction: column;
-  }
-  
+  <div class="diary">
+    <a-card>
+      <a-menu v-model:selectedKeys="current" mode="horizontal" :items="items" />
+      <div class="write">
+        <div class="title">日记本</div>
+        <a-textarea v-model:value="value1" placeholder="在此输入你的日记" auto-size />
+      </div>
+      <div class="search">
+        <div class="title">搜索</div>
+        <a-textarea v-model:value="searchline" placeholder="在此输入查找片段" auto-size />
+        <a-date-picker v-model:value="searchtime" style="margin-right: 10%;" />
+        <a-button type="primary" @click="search">搜索</a-button>
+      </div>
+    </a-card>
+  </div>
+</template>
 
-  </style>
+<script>
+export default {
+  data() {
+    return {
+      current: "写日记",
+      items: ["写日记", "查找日记", "所有日记"],
+      searchline: '',
+      searchtime: '',
+      getcontent: ''
+    };
+  },
+  methods: {
+    navigate() {
+      // 跳转
+      this.$router.push('/mainpage');
+    },
+    search() {
+      this.$axios.post('/diaries/search', { time: this.searchtime, content: this.searchline })
+        .then(response => {
+          // 处理响应数据
+          console.log(response.data);
+          this.getcontent = response.data;
+        })
+        .catch(error => {
+          // 处理错误情况
+          console.log(error);
+        });
+    },
+  },
+  created() {
+
+  },
+};
+</script>
+
+<style scoped>
+.diary {
+  background-image: url('../assets/blacktiane.jpg');
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+  position: fixed;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.write {
+  width: 39%;
+  margin-right: 10%;
+}
+
+.title {
+  font-family: 'Times New Roman', Times, serif;
+  /* 设置字体为 Times New Roman */
+  font-style: italic;
+  /* 设置字体样式为斜体 */
+  text-align: center;
+  font-size: 20px;
+}
+</style>
